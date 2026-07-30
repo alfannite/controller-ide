534f8f0131e047e3ce0c815ea1ae8c52dfeb421b
alfannite <alfannode@gmail.com>
2026-07-30 23:03:32 +0700

    Update

--- ESP32/initiative/Traffic-Light/Traffic-Light/src/main.cpp
+++ ESP32/initiative/Traffic-Light/Traffic-Light/src/main.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+void printSystemInfo() {
+    Serial.println("========================================");
+    Serial.println("        ESP32 SYSTEM INFORMATION");
+    Serial.println("========================================");
+
+    Serial.printf("Chip Model     : %s\n", ESP.getChipModel());
+    Serial.printf("Chip Revision  : %d\n", ESP.getChipRevision());
+    Serial.printf("CPU Frequency  : %d MHz\n", ESP.getCpuFreqMHz());
+    Serial.printf("Flash Size     : %d MB\n", ESP.getFlashChipSize() / (1024 * 1024));
+    Serial.printf("Flash Speed    : %d MHz\n", ESP.getFlashChipSpeed() / 1000000);
+    Serial.printf("Free Heap      : %d Bytes\n", ESP.getFreeHeap());
+    Serial.printf("SDK Version    : %s\n", ESP.getSdkVersion());
+
+    Serial.println("========================================");
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    delay(1000);
+
+    printSystemInfo();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    static uint32_t counter = 1;
+
+    Serial.printf(
+        "[%03lu] ESP32 Running | Uptime : %lu sec | Free Heap : %d Bytes\n",
+        counter++,
+        millis() / 1000,
+        ESP.getFreeHeap()
+    );
+
+    delay(1000);
 }
